ad2a676b5cdeaeaeec22dad1773fa19265fc453e
ArkSeyonet <31637812+ArkSeyonet@users.noreply.github.com>
2020-06-10 14:53:12 -0500

    Add rpchat module

---
+++ modules/__base__/rpchat/client/events.lua
@@ -0,0 +1,25 @@
+
+-- Copyright (c) Jérémie N'gadi
+--
+-- All rights reserved.
+--
+-- Even if 'All rights reserved' is very clear :
+--
+--   You shall not use any piece of this software in a commercial product / service
+--   You shall not resell this software
+--   You shall not provide any facility to install this particular software in a commercial product / service
+--   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
+--   This copyright should appear in every part of the project code
+
+onServer('rpchat:sendLifeInvaderMessage', function(playerId, message, name)
+
+  module.SendLifeInvaderMessage(playerId, message, name)
+
+end)
+
+onServer('rpchat:sendMe', function(playerId, message)
+
+  local targetPed = GetPlayerPed(GetPlayerFromServerId(playerId))
+  module.Draw3DTextOverheadWithTimeout(targetPed,'~b~*~w~'..message..'~b~*',1,0) 
+
+end)
